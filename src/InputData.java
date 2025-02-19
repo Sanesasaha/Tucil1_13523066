40021e4495c6e472623103ce401df23d37c9bfbf
Sanesasaha <13523066@std.stei.itb.ac.id>
2025-02-19 23:37:23 +0700

    feat: printPiece method

--- src/InputData.java
+++ src/InputData.java
@@ -2,5 +2,13 @@ public class InputData {
     public int N, M, P;
     public String S;
     
+    public Board B;
     public Piece[][] pieces;
+
+    public void printPieces(){
+        for(int i = 0; i < P; i++){
+            pieces[i][0].printShape();
+            System.out.println();
+        }
+    }
 }

cfc9189028f93d15b2f21898304428785af219c1
Sanesasaha <13523066@std.stei.itb.ac.id>
2025-02-19 23:41:08 +0700

    feat: board initialization

--- src/FileIO.java
+++ src/FileIO.java
@@ -37,7 +37,10 @@ public class FileIO {
             data.P = Integer.parseInt(currentLine[2]);
             data.pieces = new Piece[data.P][8];
             data.S = br.readLine();
-            
+            data.B = new Board();
+            data.B.width = data.M;
+            data.B.height = data.N;
+            data.B.initiate();
             
 
             if(data.S.equals("DEFAULT") == false){
@@ -72,13 +75,13 @@ public class FileIO {
             e.printStackTrace();
         }
 
-        System.out.println(data.M);
-        System.out.println(data.N);
-        System.out.println(data.P);
-        System.out.println(data.S);
-        for(i=0;i<data.P;i++){
-            System.out.print("[" + data.pieces[i][0].height + ", " + data.pieces[i][0].width + "] ");
-        }
+        // System.out.println(data.M);
+        // System.out.println(data.N);
+        // System.out.println(data.P);
+        // System.out.println(data.S);
+        // for(i=0;i<data.P;i++){
+        //     System.out.print("[" + data.pieces[i][0].height + ", " + data.pieces[i][0].width + "] ");
+        // }
 
         // Pembacaan bentuk setiap piece (TODOLIST: handle kasus aneh2)
         try (BufferedReader br = new BufferedReader(new FileReader(txt))){
@@ -129,6 +132,27 @@ public class FileIO {
     }
 
     public static void main(String[] args) {
-        InputData data = readInputTxt("data/", "tes.txt");
+        InputData data = readInputTxt("data/", "original.txt");
+        System.out.println("------------");
+        long start, end, exec;
+        
+        // data.B.place(data.pieces[0][0], 0, 0);
+        // data.B.place(data.pieces[1][0], 0, 1);
+        // data.B.place(data.pieces[2][0], 2, 2);
+        // data.B.printState();
+
+        // data.pieces[1][1].printShape();
+        // data.pieces[1][1].stats();
+        
+        start = System.nanoTime();
+        data.B  = Solve.BruteForce(data, 0, 0);
+        end = System.nanoTime();
+        exec = (end - start);
+
+        System.out.println();
+        data.B.printState();
+        System.out.println("status: " + data.B.status);
+        System.out.println("Time (ns): " + exec);
+        System.out.println("Total case: " + data.B.total_case);
     }
 }
